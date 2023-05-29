c840d02f7546495f3ab36b2232a7a1adcc05b3ed
Caleb Seña Melo <calebsenm@gmail.com>
2023-05-29 01:21:04 -0500

    fronted to to show data

--- main.go
+++ main.go
@@ -7,18 +7,18 @@ import(
 
 func main(){
 	
-	router := gin.Default();
-    router.Static("fronted",".fronted")
-    router.GET("/",func (c *gin.Context ){
-        c.File("./fronted/index.html");
+    router := gin.Default();
+    router.Static("src", "./src/")
+
+    router.GET("/", func (c *gin.Context ) {
+        c.File("./src/");
     });
 
-	router.GET("/tasks",tasks.GetTasks);
-	router.GET("/task:id",tasks.GetTask);
-	router.POST("/task",tasks.PostTask);
-	
-    router.PUT("/task/:id",tasks.UpdateTask);
-	router.DELETE("/task/:id",tasks.DeleteTask);
+    router.GET("/tasks",tasks.GetTasks);
+    router.GET("/task:id",tasks.GetTask);
+    router.POST("/task",tasks.PostTask);
+	router.PUT("/task/:id",tasks.UpdateTask);
+    router.DELETE("/task/:id",tasks.DeleteTask);
 
-	router.Run("localhost:8000");
+    router.Run("localhost:8000");
 }
