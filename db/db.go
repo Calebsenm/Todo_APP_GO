6080f7017ad5fbed5194dcf88cea31886a2d90b4
Caleb Seña Melo <calebsenm@gmail.com>
2023-06-06 23:10:37 -0500

    Todo Complete

--- db/db.go
+++ db/db.go
@@ -1,31 +1,29 @@
-package db 
+package db
 
 import (
-    _ "github.com/lib/pq"
-    "database/sql"
-    "fmt"
-    "log"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
 )
 
 const (
-    DB_USER = "caleb"
-    DB_PASSWORD = "12345"
-    DB_NAME = "test"
-    DB_HOST = "localhost"
-    DB_PORT = 5432
-    
+	DB_USER     = "caleb"
+	DB_PASSWORD = "12345"
+	DB_NAME     = "test"
+	DB_HOST     = "localhost"
+	DB_PORT     = 5432
 )
 
+func Connet() (*sql.DB, error) {
 
-func Connet() (*sql.DB, error ){
-    
-    connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", DB_HOST,DB_PORT, DB_USER , DB_PASSWORD , DB_NAME);
-    //open the conexion 
-    db, err := sql.Open("postgres",connStr );
-    
-    if err != nil {
-        log.Fatal("Error to Open the database " , err);
-    }
-    // return the data 
-    return db , err ;  
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
+	//open the conexion
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal("Error to Open the database ", err)
+	}
+	// return the data
+	return db, err
 }
--- db/taskModel.go
+++ db/taskModel.go
@@ -1,12 +1,11 @@
-
-package db 
+package db
 
 import (
-    "time"
+	"time"
 )
-type Task struct {
 
-    Tittle      string `json:"tittle"`
-	Text        string `json:"text"`
-    Date        time.Time `json:"date"` 
+type Task struct {
+	Tittle string    `json:"tittle"`
+	Text   string    `json:"text"`
+	Date   time.Time `json:"date"`
 }
