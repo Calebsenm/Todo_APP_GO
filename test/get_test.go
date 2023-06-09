6e9558419718d7dfd8d9c189110a8e05b07d253c
calebsenm <calebgamercraft@gmail.com>
2023-06-08 19:41:55 -0500

    ready

--- test/get_test.go
+++ test/get_test.go
@@ -1,27 +1,48 @@
-package tasks
+
+package main
 
 import (
-
-
-	"net/http"
+    "encoding/json"
+    "net/http"
 	"net/http/httptest"
 	"testing"
-	"todoApp/tasks"
+    "todoApp/db"
+    "todoApp/tasks"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+
 )
 
 func TestGetTasks(t *testing.T) {
+	// Crea un enrutador de Gin
+	router := gin.Default()
 
-	router := gin.Default();
-	router.GET("/tasks", tasks.GetTask,nil);
-	w := httptest.NewRecorder();
+	// Registra el manejador para la ruta GET /tasks
+	router.GET("/tasks", tasks.GetTasks)
 
-    req, _ := http.NewRequest("GET","/tasks",nil);
-    router 
+	// Crea un ResponseRecorder falso para capturar la respuesta
+	w := httptest.NewRecorder()
+
+	// Crea una solicitud GET de prueba para /tasks
+	req, err := http.NewRequest("GET", "/tasks", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Envía la solicitud al enrutador
+	router.ServeHTTP(w, req)
+
+	// Verifica el código de estado de la respuesta
+	assert.Equal(t, http.StatusOK, w.Code)
 
 
-
-	assert.Equal(t, http.StatusOK, w.Code);
+    // 
+    var ListTasks  []db.Task
+    json.Unmarshal(w.Body.Bytes(), &ListTasks);
+    assert.NotEmpty(t, ListTasks);
 
 }
+
+
+
+
